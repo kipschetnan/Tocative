9b71cbf062c2b04017b2674c50c3cea7b69b83fd
Kip Schetnan <kipdougschetnan@gmail.com>
2023-02-09 21:30:20 -0600

    added new model and typedefs/resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,29 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Messages } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+      me: async (parent, args, context) => {
+        if (context.user) {
+          const userData = await User.findOne({ _id: context.user._id })
+            .populate('friends');
+  
+          return userData;
+        }
+  
+        throw new AuthenticationError('Not logged in');
+      },
+      users: async () => {
+        return User.find()
+          .populate('friends');
+      },
+      user: async (parent, { username }) => {
+        return User.findOne({ username })
+          .populate('friends')
+      },
+      message: async (parent, { _id }) => {
+        return Messages.findOne({ _id });
+      }
+    }
+}
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,20 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    username: String
+    friendCount: Int
+    messages: [Messages]
+    friends: [User]
+  }
+
+  type Messages {
+    _id: ID
+    messageText: String
+    createdAt: String
+    username: String
+  }
+`
