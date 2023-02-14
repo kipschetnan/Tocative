3105bf15be0e1b4727b432c3f90b06eef9489402
kallieog <111663090+kallieog@users.noreply.github.com>
2023-02-13 20:46:04 -0600

    profile page

---
+++ client/src/pages/profile/Profile.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Profile.css'
+import Navbar from '../../components/Navbar/Navbar'
+
+const ProfilePage = () => {
+    return (
+        <div className='profileContainer'>
+            <main className='userInfo'>
+                <div className='friendList'>
+                    <h1></h1>
+                </div>
+                <div className='changeName'>
+                      
+                    
+
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default ProfilePage
