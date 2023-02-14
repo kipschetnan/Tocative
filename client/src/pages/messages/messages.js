e5e7ade16ed2361d84ead5670245250b4df44b21
Code <sangw009@umn.edu>
2023-02-13 19:30:29 -0600

    added messages page

--- client/src/pages/messages/messages.js
+++ client/src/pages/messages/messages.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import '../messages/messages.css'
+import { Link } from 'react-router-dom';
 
-export default function messages() {
+const Messages = ({ messages, username }) => {
+  if (!messages.length) {
+    return <h3>No Messages Yet</h3>;
+  }
+
   return (
     <div>
-        
+      <h3>{username}</h3>
+      {messages &&
+        messages.map(message => (
+            <div className="card-body">
+              <Link to={`/messages/${message._id}`}>
+                <p>{message.messageText}</p>
+              </Link>
+            </div>
+        ))}
     </div>
   );
-}
+};
+
+export default Messages;
