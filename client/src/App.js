e1ab7b470e4be19cb09865f2decc0be6e41a1f84
Kip Schetnan <kipdougschetnan@gmail.com>
2023-02-16 18:37:39 -0600

    login/register page

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,8 @@ import LoginPage from './pages/login/LoginPage';
 import RegisterPage from './pages/register/RegisterPage';
 import HomePage from './pages/home/HomePage';
 import Chats from './pages/chats/Chats';
-import Messages from './pages/messages/messages'
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import Messages from './pages/messages/Messages'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import ProfilePage from './pages/profile/Profile';
 import { io } from 'socket.io-client'
 
