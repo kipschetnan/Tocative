51881b3051e97975346141845d29ec8b910dda95
Ksubi 1k <xksubi1x@gmail.com>
2023-02-20 18:15:37 -0600

    security

--- client/src/pages/chats/Chats.js
+++ client/src/pages/chats/Chats.js
@@ -2,21 +2,29 @@ import React, { useState } from 'react'
 import './Chats.css'
 import Search from '../../components/searchBar/Search'
 import Chat from '../../components/chatBox/Chat'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import {ADD_FRIEND} from '../../utils/mutations'
 import { useMutation, useQuery } from '@apollo/client';
 import Auth from '../../utils/auth'
 import { QUERY_USER_CONVERSATIONS } from '../../utils/queries'
 import Footer from '../../components/footer/Footer'
 
+
 const Chats = () => {
+
+  const navigate = useNavigate()
 
   const [formState, setFormState] = useState({ username: ''})
   const [addFriend, { error }] = useMutation(ADD_FRIEND)
   const { loading: userConvoLoading, error: userConvoError, data: userConvoData } = useQuery(QUERY_USER_CONVERSATIONS);
 
   if (userConvoLoading) return <p>Loading logged in user...</p>;
-  if (userConvoError) return <p>Error loading logged in user: {userConvoError.message}</p>;
+  if (userConvoError) {
+    navigate('/login')
+    return <p>Error loading logged in user: {userConvoError.message}</p>;
+  }
+  
+  
 
 
   // console.log(userConvoData.userConversations)
@@ -55,21 +63,15 @@ const Chats = () => {
               <button className="searchSubmit" onClick={onSubmit} > Submit </button>
             </div>
 
-            <h3 id="label" className='listTitle'>My conversations:</h3>
-            <div className='list'>
-              
-              {convos.map((convo) => {
-                console.log(convo.name)
-                return <Chat name={convo.name} id={convo._id}/>
-              })}
 
             <div className='convosWrapper'>
 
               <h3 id="label" className='listTitle'>My conversations:</h3>
               <div className='list'>
 
-                {userConvoData.userConversations.map((convo) => {
-                  return <Chat  />
+                {convos.map((convo) => {
+                  console.log(convo.name)
+                  return <Chat name={convo.name} id={convo._id}/>
                 })}
 
               </div>
